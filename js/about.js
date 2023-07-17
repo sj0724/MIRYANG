deafd907529343154d75f376d9e0689fcc207bbc
sj0724 <137033202+sj0724@users.noreply.github.com>
2023-07-17 18:19:26 +0900

    Add files via upload

--- js/about.js
+++ js/about.js
@@ -3,6 +3,7 @@ const cross = document.querySelector('.cross');
 const title = document.querySelector('.title');
 const content = document.querySelector('#content');
 const up = document.querySelector('.up_sign');
+const sideMenu = document.querySelector('.side_bar');
 
 function rotate(){
     cross.style.rotate = '0deg';
@@ -17,10 +18,17 @@ function restart(){
 function scroll(){
     window.scrollTo({
         top:0, left:0, behavior:'smooth' 
-    })
+    });
 };
 
+var scrollSide = document.querySelector('.explane_2').offsetTop;
+
+function move(){
+    window.scrollTo({top:scrollSide - 150, behavior:'smooth'
+    });
+}
 
 menu.addEventListener('mouseover', rotate);
 content.addEventListener('mouseover', restart);
 up.addEventListener('click', scroll);
+sideMenu.addEventListener('click',move)
