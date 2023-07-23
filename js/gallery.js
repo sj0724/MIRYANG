0440527e72270e496b1d581719bdf2390c79a265
sj0724 <137033202+sj0724@users.noreply.github.com>
2023-07-23 23:01:02 +0900

    Add files via upload

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,7 @@ const sidemenu = document.querySelector('.side_menu');
 const menu = document.querySelector('.menu');
 const content = document.querySelector('#contents');
 const search_tab = document.querySelector('.search_tab');
+const logo = document.querySelector('.logo');
 
 function zoom(e){
     var i = e.target.style.backgroundImage;
@@ -23,19 +24,19 @@ var u = menu.nodeValue = 0;
 function side_out(e){
     if(e.target.parentNode.className == 'side_menu') return;
     if(e.target.parentNode.className == 'logo') return;
+    if(e.target.parentNode.className == 0 ) return;  
     if(u == false){
         menu.classList.add('small');
         search_tab.classList.add('small');
+        logo.children[0].style.writingMode = 'vertical-rl';
         u =+ 1;
     }else if(u == true){
         menu.classList.remove('small');
         search_tab.classList.remove('small');
+        logo.children[0].style.writingMode = '';
         u = 0;
     };
 };
-
-
-
 
 picture.addEventListener('click', zoom);
 popUp.addEventListener('click', zoom_out);
