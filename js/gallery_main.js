01043c8034f65134b4b2f67939adfdef04b8bce1
sj0724 <137033202+sj0724@users.noreply.github.com>
2023-07-25 21:44:25 +0900

    Add files via upload

---
+++ js/gallery_main.js
@@ -0,0 +1,27 @@
+const sidemenu = document.querySelector('.side_menu');
+const menu = document.querySelector('.menu');
+const content = document.querySelector('#contents');
+const search_tab = document.querySelector('.search_tab');
+const logo = document.getElementsByName("pop_up");
+
+var u = menu.nodeValue = 0;
+
+function side_out(){
+    if(u == 0){
+        menu.classList.add('small');
+        search_tab.classList.add('small');
+        menu.children[0].style.writingMode = 'vertical-rl';
+        menu.children[0].style.fontSize = '15px';
+        u =+ 1;
+    }else if(u == 1){
+        menu.classList.remove('small');
+        search_tab.classList.remove('small');
+        menu.children[0].style.writingMode = '';
+        menu.children[0].style.fontSize = '30px';
+        u = 0;
+    };
+};
+
+console.log(logo);
+
+logo.forEach(Element => Element.addEventListener('click', side_out));
